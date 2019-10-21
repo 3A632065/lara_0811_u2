64a6bd94ec6a548ed8ec0d308482540924704bc1
xinru <s3a632065@student.ncut.edu.tw>
2019-10-21 21:37:47 +0800

    練習4-2 增加一個連結(修改)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
+                    Laravel
                     <a href="{{ route('hello.index') }}">Hello</a>
                 </div>
 
@@ -92,6 +93,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('hello.index') }}">Hello</a>
                 </div>
             </div>
         </div>
